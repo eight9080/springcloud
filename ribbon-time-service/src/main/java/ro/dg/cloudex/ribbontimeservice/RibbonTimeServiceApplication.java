a451fc9428d8b74246ff6bbeb17560a87340886c
eight9080 <eight9080@gmail.com>
2018-02-06 00:46:31 +0200

    ribbon load balance

---
+++ ribbon-time-service/src/main/java/ro/dg/cloudex/ribbontimeservice/RibbonTimeServiceApplication.java
@@ -0,0 +1,30 @@
+package ro.dg.cloudex.ribbontimeservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class RibbonTimeServiceApplication {
+
+	@Value("${server.port}")
+	private String port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String getTime(){
+		return "Current time is "+ LocalDate.now().toString() +" (answered by service running on "+port+")";
+	}
+
+
+}
